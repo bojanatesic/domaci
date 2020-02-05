9a57b0aa82bb022643076ed5176de78e67c71b4b
Bojana Tesic <tesic.bojana@yahoo.com>
2020-02-05 10:16:24 +0100

    Dodati zadatak3.js i zadatak4.js

---
+++ Nedelja2/zadatak3.js
@@ -0,0 +1,6 @@
+let i=20;
+
+    if(i%3==0){
+        console.log('Broj ' + i + ' je deljiv sa 3');    
+    }
+    else console.log('Broj ' + i + ' nije deljiv sa 3');    
---
+++ Nedelja2/zadatak4.js
@@ -0,0 +1,7 @@
+let x=0;
+let brojac=0
+for (x=5; brojac<1000; x++)
+if(x%5==0){
+console.log(x);
+brojac++;
+}
