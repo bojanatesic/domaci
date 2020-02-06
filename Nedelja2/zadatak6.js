f015f36a6c8cedca53f8a169885ef8b828e887ca
Bojana Tesic <tesic.bojana@yahoo.com>
2020-02-06 18:48:51 +0100

    Dodati zadatak5.js i zadatak6.js

--- Nedelja2/zadatak6.js
+++ Nedelja2/zadatak6.js
@@ -1,48 +1,28 @@
-let i=0;
-let brojac =0
-for (i>0; brojac<=100; i++){
-switch(i){
-    case 1:
-        if(i%3==0 && i%5==0){
-        console.log ('FizzBuzz');}
-        brojac++;
-
-        // else {
-        //     console.log(i);
-        // }
-            break;
-    case 2:
-        if (i%3==0){
-        console.log ('Fizz');}
-        brojac++;
-
-        // else {
-        //     console.log (i)
-        // }
-        break;
-    case 3:
-        if (i%5==0){
-        console.log ('Buzz');}
-        brojac++;
-
-        // else{
-        //     console.log(i);
-        // }
-        break;
-    case 4:
-        if(i%3!=0 && i%5!=0){
-        console.log ('Fizz');}
-        brojac++;
-
-        // else{
-        //     console.log(i);
-        // }
-        break;
-        
-    default: console.log(i);
-    brojac++;
-    break;
+// let ispis=["FizzBuzz", "Fizz", "Buzz"]
+// let i =0;
+// let brojac =0;
+// for (i=1; brojac<100; i++){
+//     if (i%3==0 && i%5==0){
+//     console.log (ispis[0])
+// } else if (i%3==0){
+//     console.log  (ispis [1])
+// } else if (i%5==0){
+//     console.log  (ispis [2])
+// } else if (i%3!=0 && i%5!=0){
+//     console.log  (i)}
+//     brojac++
 // }
-}
 
+let i =0;
+let brojac =0;
+for (i=1; brojac<100; i++){
+    if (i%3==0 && i%5==0){
+    console.log ('FizzBuzz')
+} else if (i%3==0){
+    console.log  ('Fizz')
+} else if (i%5==0){
+    console.log  ('Buzz')
+} else if (i%3!=0 && i%5!=0){
+    console.log  (i)}
+    brojac++
 }
