f47b589ed6e74b5e9cd6c5a9368b0bf589dd575f
Bojana Tesic <tesic.bojana@yahoo.com>
2020-02-11 22:34:12 +0100

    Dodati zadaci Nedelja3 Drugi dan

---
+++ Nedelja3/drugiDanZadatak1.js
@@ -0,0 +1,10 @@
+function najveciBroj(broj1, broj2, broj3) {
+    if (broj1 > broj2 && broj1 > broj3)
+        return broj1;
+    else if (broj2 > broj1 && broj2 > broj3)
+        return broj2;
+    else if (broj3 > broj1 && broj3 > broj2)
+        return broj3;
+}
+let maksimum = najveciBroj(4, 8, 5)
+console.log(maksimum)
---
+++ Nedelja3/drugiDanZadatak2.js
@@ -0,0 +1,14 @@
+function cenaPicePoCm2 (r,cena){
+    povrsina=Math.pow(r,2)*Math.PI
+    cenaPoCm2 =cena/povrsina
+    console.log(cenaPoCm2)
+}
+    cenaPicePoCm2(60,1200)
+
+// function cenaPicePoCm2(r, cena) {
+//     povrsina = Math.pow(r, 2) * Math.PI
+//     cenaPoCm2 = cena / povrsina
+//     return cenaPoCm2
+// }
+// let rezultat = cenaPicePoCm2(60, 1200)
+// console.log(rezultat)
